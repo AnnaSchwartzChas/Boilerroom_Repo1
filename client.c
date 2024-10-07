ba7ccc506df2414afe21e23f51e07620766233e6
AnnaSchwartzChas <anna.schwartz@chasacademy.se>
2024-10-07 15:54:55 +0200

    Client Server Windows

--- client.c
+++ client.c
@@ -1,47 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")  // Länka med Winsock-biblioteket
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
 int main() {
-    int sock = 0;
+    WSADATA wsaData;
+    SOCKET sock = INVALID_SOCKET;
     struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
     const char *hello = "Hello from client";
 
-    // Creating socket file descriptor
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
+    // Initialisera Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup failed. Error Code: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    // Skapa socket-filbeskrivare
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        printf("\nSocket creation error. Error Code: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
     }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
+    // Konvertera IPv4-adressen från text till binär form
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
-        return -1;
+        closesocket(sock);
+        WSACleanup();
+        return 1;
     }
 
-    // Connecting to the server
+    // Anslut till servern
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection failed \n");
-        return -1;
+        printf("\nConnection failed. Error Code: %d\n", WSAGetLastError());
+        closesocket(sock);
+        WSACleanup();
+        return 1;
     }
 
-    // Sending message to the server
+    // Skicka meddelande till servern
     send(sock, hello, strlen(hello), 0);
     printf("Hello message sent\n");
 
-    // Receiving response from server
-    read(sock, buffer, BUFFER_SIZE);
+    // Ta emot svar från servern
+    recv(sock, buffer, BUFFER_SIZE, 0);
     printf("Server: %s\n", buffer);
 
-    close(sock);
+    // Stäng anslutningen och Winsock
+    closesocket(sock);
+    WSACleanup();
     return 0;
 }
--- server.c
+++ server.c
@@ -1,63 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")  // Länka med Winsock-biblioteket
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
 int main() {
+    WSADATA wsaData;
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
     const char *hello = "Hello from server";
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
+    // Initialisera Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup failed. Error Code: %d\n", WSAGetLastError());
+        return 1;
     }
 
-    // Configuring server address
+    // Skapa socket-filbeskrivare
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        printf("Socket failed. Error Code: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    // Konfigurera serveradress
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Binding socket to the specified port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    // Binda socket till angiven port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == SOCKET_ERROR) {
+        printf("Bind failed. Error Code: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
     }
 
-    // Listening for incoming connections
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    // Lyssna efter inkommande anslutningar
+    if (listen(server_fd, 3) == SOCKET_ERROR) {
+        printf("Listen failed. Error Code: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
     }
 
     printf("Server is listening on port %d\n", PORT);
 
-    // Accepting incoming connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
-        perror("Accept failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    // Acceptera inkommande anslutning
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) == INVALID_SOCKET) {
+        printf("Accept failed. Error Code: %d\n", WSAGetLastError());
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
     }
 
-    // Reading data from client
-    read(new_socket, buffer, BUFFER_SIZE);
+    // Läsa data från klienten
+    recv(new_socket, buffer, BUFFER_SIZE, 0);
     printf("Client: %s\n", buffer);
 
-    // Sending response to client
+    // Skicka svar till klienten
     send(new_socket, hello, strlen(hello), 0);
     printf("Hello message sent\n");
 
-    close(new_socket);
-    close(server_fd);
+    closesocket(new_socket);
+    closesocket(server_fd);
+    WSACleanup();  // Stäng ner Winsock
     return 0;
 }
-
